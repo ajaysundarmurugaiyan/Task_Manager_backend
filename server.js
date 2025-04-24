2ebacbd28b4f4dbdeed4bb62a37292f1a3423fc5
AJAY SUNDAR <139756597+ajaysundarmurugaiyan@users.noreply.github.com>
2025-04-24 07:36:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const authRoutes = require('./routes/auth');
 const app = express();
 
 // CORS configuration
-const allowedOrigins = ['https://taks-manager-frontend.vercel.app/'];
+const allowedOrigins = ['https://taks-manager-frontend.vercel.app','http://localhost:5173'];
 
-app.use(cors(
+//app.use(cors(
 //   {
 //   origin: function(origin, callback) {
 //     // Allow requests with no origin (like mobile apps or curl requests)
@@ -23,7 +23,25 @@ app.use(cors(
 //   },
 //   credentials: true
 // }
-));
+//));
+
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+
+  // Respond to preflight requests
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
